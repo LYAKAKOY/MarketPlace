a3448e91635585cda46199083fad40766eaefca7
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-04 23:40:09 +0300

    change tag product => products

--- backend/main.py
+++ backend/main.py
@@ -5,5 +5,5 @@ from api.products.handlers import products_router
 app = FastAPI(description="The Best MarketPlace")
 
 main_api_router = APIRouter()
-main_api_router.include_router(products_router, prefix="/product", tags=["product"])
+main_api_router.include_router(products_router, prefix="/products", tags=["products"])
 app.include_router(main_api_router)
