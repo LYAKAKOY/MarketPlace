d1ebcbc07287ef0617a2ef373c75345af3f5a785
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-04 23:02:34 +0300

    add session client elastic

--- backend/db/elasticsearch/session.py
+++ backend/db/elasticsearch/session.py
@@ -2,7 +2,7 @@ from typing import Generator
 import settings
 from elasticsearch import AsyncElasticsearch
 
-async def _get_db_es() -> Generator:
+async def get_db_es() -> Generator:
     elastic_client = AsyncElasticsearch(hosts=settings.ES_DATABASE_URL)
     try:
         yield elastic_client
