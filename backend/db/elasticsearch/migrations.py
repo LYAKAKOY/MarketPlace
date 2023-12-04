85b596771637229d43069f37dda1490d3980e931
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-04 23:03:30 +0300

    migrations for elastic

--- backend/db/elasticsearch/migrations.py
+++ backend/db/elasticsearch/migrations.py
@@ -4,10 +4,14 @@ from elasticsearch import AsyncElasticsearch
 from indexes import all_indexes
 
 es_database_url = f"http://{os.environ.get('ES_DATABASE')}:{os.environ.get('ES_PORT')}"
+DEFAULT_SETTINGS_INDEXES = {
+    "number_of_shards": 3,
+    "number_of_replicas": 2
+}
 
 async def create_index(name_index: str, mappings: dict):
     elastic_client = AsyncElasticsearch(hosts=es_database_url)
-    await elastic_client.indices.create(index=name_index, mappings=mappings)
+    await elastic_client.indices.create(index=name_index, mappings=mappings, settings=DEFAULT_SETTINGS_INDEXES)
     await elastic_client.close()
 
 async def migrations():
