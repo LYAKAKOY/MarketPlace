6e984ddefd4fbfe3113965d7e8f88635250783e9
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-04 23:24:29 +0300

    add exclude additionalProp1

--- backend/api/actions/products.py
+++ backend/api/actions/products.py
@@ -1,10 +1,21 @@
+from typing import List
+
 from elasticsearch import AsyncElasticsearch
 
 from api.products.schemas import CreateProduct, ShowProduct
 
 
 async def _create_product(product: CreateProduct, elastic_client: AsyncElasticsearch) -> ShowProduct | None:
-    res = await elastic_client.index(index="products", document=product.model_dump(exclude_none=True))
+    res = await elastic_client.index(index="products", document=product.model_dump(exclude_none=True,
+                                                                        exclude={'additionalProp1'}))
     if res.meta.status == 201:
         return ShowProduct(id_product=res.get("_id"), **product.model_dump(exclude_none=True,
                                                                         exclude={'additionalProp1'}))
+
+async def _get_all_products(elastic_client: AsyncElasticsearch) -> List[ShowProduct] | None:
+    res = await elastic_client.search(index="products", query={"match_all": {}})
+    products = res.get("hits").get("hits")
+    if products is not None:
+        list_show_products = [ShowProduct(id_product=product.get("_id"), **product.get("_source"))
+                              for product in products]
+        return list_show_products
