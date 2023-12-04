5947d94d5d4d4fb3c01cf8bc87891aaba33212cd
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-04 23:05:56 +0300

    add api router products to main

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+
+from api.products.handlers import products_router
 
 app = FastAPI(description="The Best MarketPlace")
+
+main_api_router = APIRouter()
+main_api_router.include_router(products_router, prefix="/product", tags=["product"])
+app.include_router(main_api_router)
