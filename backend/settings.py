84dd7883315532ef819834811e583702b75d795a
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-07 15:44:00 +0300

    add scroll time

--- backend/settings.py
+++ backend/settings.py
@@ -4,7 +4,12 @@ from envparse import Env
 
 env = Env()
 
-ES_DATABASE_URL = env.str(
+ES_DATABASE_URL: str = env.str(
     "ES_DATABASE_URL",
     default=f"http://{os.environ.get('ES_DATABASE')}:{os.environ.get('ES_PORT')}",
+)
+
+SCROLL_TIME: str = env.str(
+    "SCROLL_TIME",
+    default="1h",
 )
