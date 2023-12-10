1905bcf76979843836b862204fb8e5c2e3c85f38
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-11 02:40:09 +0300

    fix clean index

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,8 @@
 import asyncio
+import os
 from typing import Generator, Any
 
-from elasticsearch import AsyncElasticsearch
-from tests.test_data import products
+from elasticsearch import AsyncElasticsearch, Elasticsearch
 import settings
 from db.elasticsearch.indexes import NAME_INDEX_PRODUCTS
 from db.elasticsearch.session import get_db_es
@@ -18,7 +18,9 @@ def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
-
+@pytest.fixture(scope="session", autouse=True)
+async def run_migrations():
+    os.system("python db/elasticsearch/migrations.py")
 @pytest.fixture(scope="function")
 async def client() -> Generator[AsyncClient, Any, None]:
     """
@@ -28,15 +30,24 @@ async def client() -> Generator[AsyncClient, Any, None]:
     app.dependency_overrides[get_db_es] = get_test_db_es
     async with AsyncClient(app=app, base_url="http://127.0.0.1") as client:
         yield client
-
 @pytest.fixture(scope="function")
-async def create_products():
-    test_elastic_client = AsyncElasticsearch(hosts=settings.ES_DATABASE_URL)
-    for test_product in products:
-        await test_elastic_client.index(index=NAME_INDEX_PRODUCTS, document=test_product)
-    return products
+def test_client_es():
+    return AsyncElasticsearch(hosts=settings.ES_DATABASE_URL)
 
 @pytest.fixture(scope="function", autouse=True)
 async def clean_index():
-    test_elastic_client = AsyncElasticsearch(hosts=settings.ES_DATABASE_URL)
-    await test_elastic_client.delete_by_query(index=NAME_INDEX_PRODUCTS, query={"match_all": {}}, params={"refresh": "true"})
+    client_es = Elasticsearch(hosts=settings.ES_DATABASE_URL)
+    client_es.options(ignore_status=[400,404]).indices.delete(index='products')
+
+products_data = [
+                {"id_company": 1, "product_name": "Product A", "description": "Description A", "category": "Category X", "sum": 100},
+                {"id_company": 2, "product_name": "Product A", "description": "Description B", "category": "Category Y", "sum": 150},
+                {"id_company": 3, "product_name": "Product C", "description": "Description C", "category": "Category Z", "sum": 200},
+                {"id_company": 1, "product_name": "Product B", "description": "Description D", "category": "Category X", "sum": 120},
+                {"id_company": 5, "product_name": "Product E", "description": "Description E", "category": "Category Y", "sum": 180},
+                {"id_company": 2, "product_name": "Product A", "description": "Description F", "category": "Category Z", "sum": 220},
+                {"id_company": 3, "product_name": "Product G", "description": "Description G", "category": "Category X", "sum": 130},
+                {"id_company": 8, "product_name": "Product A", "description": "Description H", "category": "Category Y", "sum": 170},
+                {"id_company": 9, "product_name": "Product C", "description": "Description I", "category": "Category Z", "sum": 240},
+                {"id_company": 3, "product_name": "Product B", "description": "Description J", "category": "Category X", "sum": 110},
+            ]
