ed508ec0466c2ab66b95d6886f5cdedf1e737e28
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-11 21:49:26 +0300

    refresh true for create product

--- backend/api/actions/products.py
+++ backend/api/actions/products.py
@@ -7,7 +7,7 @@ from db.elasticsearch.indexes import NAME_INDEX_PRODUCTS
 
 
 async def _create_product(product: CreateProduct, elastic_client: AsyncElasticsearch) -> ShowProduct | None:
-    res = await elastic_client.index(index=NAME_INDEX_PRODUCTS, document=product.model_dump(exclude_none=True))
+    res = await elastic_client.index(index=NAME_INDEX_PRODUCTS, document=product.model_dump(exclude_none=True), refresh=True)
     if res.meta.status == 201:
         return ShowProduct(id_product=res.get("_id"), **product.model_dump(exclude_none=True))
 
@@ -45,7 +45,7 @@ async def _get_all_product_by_company_id(id_company: int, elastic_client: AsyncE
             return ScrollListProducts(scroll_id=scroll_id, products=list_show_products)
 
 async def _get_products_by_scroll(scroll_id: str, elastic_client: AsyncElasticsearch) -> ScrollListProducts | None:
-    res = await elastic_client.scroll(scroll_id=scroll_id, scroll=settings.SCROLL_TIME)
+    res = await elastic_client.options(ignore_status=400).scroll(scroll_id=scroll_id, scroll=settings.SCROLL_TIME)
     if res.meta.status == 200:
         products = res.get("hits").get("hits")
         if products:
