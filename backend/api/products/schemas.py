58ff4d52accef68eb8aad8f9e9622849a4f2c8a1
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-16 01:00:15 +0300

    delete class Config and set model config

--- backend/api/products/schemas.py
+++ backend/api/products/schemas.py
@@ -1,31 +1,26 @@
 from typing import List
 
 from api.base_schemas import TunedModel
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Extra
 
 
 class CreateProduct(BaseModel):
+    model_config = ConfigDict(strict=True, extra="allow")
     id_company: int
     product_name: str
     description: str
     category: str
     sum: int
 
-    class Config:
-        strict = True
-        extra = "allow"
-
 
 class ShowProduct(TunedModel):
+    model_config = ConfigDict(extra="allow")
     id_product: str
     id_company: int
     product_name: str
     description: str
     category: str
     sum: int
-
-    class Config:
-        extra = "allow"
 
 
 class ScrollListProducts(TunedModel):
